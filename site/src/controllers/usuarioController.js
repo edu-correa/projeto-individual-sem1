37d91a668783017db859a396e51c54212b3232b1
edu-correa <eduardo.correa@sptech.school>
2023-06-01 15:29:11 -0300

    Alterei muita coisa

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -1,6 +1,5 @@
 var usuarioModel = require("../models/usuarioModel");
 
-var sessoes = [];
 
 function testar(req, res) {
     console.log("ENTRAMOS NA usuarioController");
@@ -65,7 +64,8 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-
+    var imagem = req.body.imagemServer;
+    console.log("Imagem: " + imagemx    )
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
@@ -73,10 +73,11 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
+    } else if (imagem == undefined) {
+        res.status(400).send("Imagem inválida")  
     } else {
-        
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, imagem)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- site/src/database/bdAcoustic.sql
+++ site/src/database/bdAcoustic.sql
@@ -6,7 +6,8 @@ CREATE TABLE usuario (
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 	nomeUsuario VARCHAR(50),
 	emailUsuario VARCHAR(50),
-	senhaUsuario VARCHAR(50)
+	senhaUsuario VARCHAR(50),
+	imagemUsuario varchar(300)
 );
 
 
